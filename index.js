9524bc7f342debd9b8e1116cb47981a5da488b3f
=emre <=yalcnnemre@gmail.com>
2021-06-23 23:04:57 +0300

    Skills ve services sectionları oluşturuldu

--- index.js
+++ index.js
@@ -75,10 +75,75 @@ skillsHeader.forEach((el)=>{
             this.parentNode.classList.remove("skills__close")
             this.parentNode.classList.add("skills__open")
         }
-        else if(classname=="skills__content skills__open")
+        else if(classname=="skills__content skills__open") 
         {
             this.parentNode.classList.remove("skills__open")
             this.parentNode.classList.add("skills__close")
         }
     })
 })
+
+const tabs=document.querySelectorAll("[data-target]")
+,tabContents=document.querySelectorAll("[data-content]")
+
+
+tabs.forEach(tab=>{
+    tab.addEventListener("click",()=>{
+        const target=document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabcontent=>{
+            tabcontent.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+        tabs.forEach(tab=>{
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+    })
+}) 
+
+
+/* const qualbutton=document.querySelectorAll(".qualification__button")
+
+const qualContent=document.querySelectorAll(".qualification__content")
+const cont=document.querySelectorAll("[data-content]")
+const tar=document.querySelectorAll("[data-target]")
+console.log(tar)
+qualbutton.forEach((el)=>{
+    el.addEventListener("click",function(){
+        
+       let target=document.querySelector(el.dataset.target)
+       
+       cont.forEach((el)=>{
+           el.classList.remove("qualification__active")
+       })
+
+       target.classList.add("qualification__active")
+
+    })
+}) */
+
+
+ const modalViews=document.querySelectorAll(".services__modal"),
+    modalBtns=document.querySelectorAll(".services__button"),
+    modalCloses=document.querySelectorAll(".services__modal-close")
+
+
+let modal=function(modalClick){
+    modalViews[modalClick].classList.add("active-modal")
+}
+
+modalBtns.forEach((modalBtn,i)=>{
+    modalBtn.addEventListener("click",()=>{
+        console.log(i)
+        modal(i)
+    })
+})
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener("click",()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove("active-modal")
+        })
+    })
+}) 
+
+
